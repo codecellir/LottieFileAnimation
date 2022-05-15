363586d36caa8699e16369980142d200e0fc13e3
Sohrab Gheisari <sohrabgheisari@hotmail.com>
2022-05-15 22:50:01 +0430

    init

---
+++ LottieAnimation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LottieAnimation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
